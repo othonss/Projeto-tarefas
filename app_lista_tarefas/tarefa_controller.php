0c974e07aa34fa9e941d1a12e95dc6c2eb20a8e2
othonss <othon.santos.silva@gmail.com>
2022-02-20 17:12:29 -0300

    Dando um feedback melhor após a inserção de registros

--- app_lista_tarefas/tarefa_controller.php
+++ app_lista_tarefas/tarefa_controller.php
@@ -4,9 +4,6 @@
     require "app_lista_tarefas/tarefa.service.php";
     require "app_lista_tarefas/conexao.php";
 
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
     //Segundo o instaciamento das classes, com seus respectivos parâmetros, caso houver.
     $tarefa = new Tarefa();
     $tarefa->__set('tarefa', $_POST['tarefa']);
@@ -16,7 +13,6 @@
     $tarefaService = new TarefaService($conexao, $tarefa);
     $tarefaService->inserir();
 
-    echo '<pre>';
-    print_r($tarefaService);
-    echo '</pre>';
+    //Ao inserir um novo registro, redirecionar o usuário de volta a página de nova tarefa
+    header('Location: nova_tarefa.php?inclusao=1');
 ?>
